be01f771b7171c42e62a2558a34a06521e46f1d4
Maxix <spir1n.maks@yandex.ru>
2025-02-26 19:58:56 +0300

    Added items 2, 3, 4 to the README.md, complete 1 quest

---
+++ src/main/java/Hello.java
@@ -0,0 +1,15 @@
+/** Start programm. */
+public final class Hello {
+
+  private Hello() {
+    throw new UnsupportedOperationException();
+  }
+  /**
+   * Запускает программу.
+   *
+   * @param args Аргументы командной строки.
+   */
+  public static void main(final String[] args) {
+    System.out.println("Hello, Java!");
+  }
+}
